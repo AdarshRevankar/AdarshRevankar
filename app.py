e9ff2571abba509a2ab5b42397311e1b083852a3
AdarshRevankar <adarsh_revankar@live.com>
2022-11-20 21:42:56 +0530

    - tech, hobbies page added + fixes

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+from crypt import methods
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/", methods = ['POST', 'GET'])
+def show_index():
     return render_template("index.html", visit_count=100)
+
+@app.route("/tech", methods = ['POST', 'GET'])
+def show_tech():
+    print("Request")
+    return render_template("tech.html")
+
+@app.route("/hobbies", methods = ['POST', 'GET'])
+def show_hobbies():
+    return render_template("hobbies.html")
 
 
 if __name__ == "__main__":
